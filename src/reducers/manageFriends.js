8d2d6f8542d8a97a5bb8c53aaaa981dc16ce8f13
Lupita <lupitarivera8899@gmail.com>
2021-09-08 12:39:10 -0500

    commit tree b9c53724d32f9a292e1a4c5a9c073c69073f88d8

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,45 @@
-export function manageFriends(state, action){
+
+
+
+
+export function manageFriends(state = {friends: []}, action){
+    // the initial state should be an object with a key, friends, set to an empty array.
+  
+    
+
+    switch(action.type){
+       case  "ADD_FRIEND":
+
+           return{
+           ...state,
+           friends: [
+               ...state.friends,
+               action.friend
+           ]
+          };
+        case "REMOVE_FRIEND":
+            const removalIndex = state.friends.findIndex(friend => friend.id === action.id)
+            return{
+               ...state,
+               friends: [
+                ...state.friends.slice(0,removalIndex),
+                ...state.friends.slice(removalIndex +1) 
+               ]
+            }
+            default:
+                return state
+        
+    }
+
 }
+
+
+// switch(action.type){
+//     case "INCREASE":
+//     return {
+//         numberOfPresents: state.numberOfPresents +1
+//     }
+//     default:
+//             return state
+// }
+// }
